a71ba30da156a6162ede21d5871fe8000fdbe8fa
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-07-23 17:16:42 +0100

    apply initial turborepo scaffolding

---
+++ library/workers-swr/tests/placeholder.test.js
@@ -0,0 +1,6 @@
+import { describe, expect, test } from 'vitest';
+describe('should error', () => {
+    test('2 is not equal 1', () => {
+        expect(2).toEqual(1);
+    });
+});
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		project: true,
+	},
+	plugins: ['@typescript-eslint', 'deprecation'],
+	rules: {
+		'no-case-declarations': 'error',
+		'no-console': 'error',
+		'prefer-const': 'error',
+		eqeqeq: 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/consistent-type-imports': 'error',
+
+		// Promises & async/await
+		'no-async-promise-executor': 'error',
+		'no-promise-executor-return': 'error',
+		'no-return-await': 'error',
+		'@typescript-eslint/await-thenable': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/promise-function-async': 'error',
+
+		'deprecation/deprecation': 'error',
+	},
+	ignorePatterns: ['vitest.config.ts'],
+};
