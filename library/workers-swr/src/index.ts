73967fdeec08f742694b88d81fc75b00e23ce567
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-07-23 17:31:38 +0100

    setup withSWR as noop

--- library/workers-swr/src/index.ts
+++ library/workers-swr/src/index.ts
@@ -1,3 +1,5 @@
-export function hello() {
-	return 'hello!';
+export function withSWR<Env extends unknown>(fetchHandler: ExportedHandlerFetchHandler<Env, unknown>): ExportedHandlerFetchHandler {
+	return async (request: Request, env: unknown, ctx: ExecutionContext): Promise<Response> => {
+		return  fetchHandler(request as Parameters<ExportedHandlerFetchHandler>[0], env as Env, ctx);
+	};
 }
